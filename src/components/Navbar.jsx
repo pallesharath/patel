14f4c9c75ae5dccd4fe7a4d18d65b0c364d302e9
satheeshpatel1 <satheeshpatel1@gmail.com>
2026-03-16 10:49:04 +0530

    remove node_modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import logo from "../assets/logo.png"
-import "./Navbar.css"
+import "./navbar.css"
 
 function Navbar() {
 
